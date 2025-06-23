5cb29dcab74bdd75ffd3df6fb2a42d4d42beed21
ZAIBITECH1 <127785281+ZAIBITECH1@users.noreply.github.com>
2025-06-23 15:01:37 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ const fs = require("fs");
 require("dotenv").config();
 
 let config = {
-    prefix: process.env.PREFIX || ".",
-    ownerName: process.env.OWNER_NAME || "𝑻𝒐𝒙𝒙𝒊𝒄-𝑺𝒂𝒏",
-    ownerNumber: process.env.OWNER_NUMBER || "2347042081220",
+    prefix: process.env.PREFIX || "~",
+    ownerName: process.env.OWNER_NAME || "Zaibi Rao",
+    ownerNumber: process.env.OWNER_NUMBER || "923039833847",
     mode: process.env.MODE || "private",
-    region: process.env.REGION || "Nigeria",
-    botName: process.env.BOT_NAME || "𝐑𝐢𝐚𝐬 𝐆𝐫𝐞𝐦𝐨𝐫𝐲",
+    region: process.env.REGION || "Pakistan",
+    botName: process.env.BOT_NAME || "Rias Bot",
     exifPack: process.env.EXIF_PACK || "𝐑𝐢𝐚𝐬 𝐆𝐫𝐞𝐦𝐨𝐫𝐲",
     exifAuthor: process.env.EXIF_AUTHOR || "𝑴𝒂𝒅𝒆 𝑩𝒚 𝑻𝒐𝒙𝒙𝒊𝒄",
     timeZone: process.env.TIME_ZONE || "Africa/Lagos",
@@ -15,7 +15,7 @@ let config = {
     autoRead: process.env.AUTO_READ?.toLowerCase() === "true" || false,
     autoViewStatus: process.env.AUTO_VIEW_STATUS?.toLowerCase() === "true" || false,
     autoReact: process.env.AUTO_REACT?.toLowerCase() === "true" || false,
-    sessionId: process.env.SESSION_ID || "",
+    sessionId: process.env.SESSION_ID || "https://session-toxxictech.zone.id/download-session?id=A9lq",
     autoRejectEnabled: process.env.AUTO_REJECT_ENABLED?.toLowerCase() === "true" || false,
     antiDelete: process.env.ANTIDELETE?.toLowerCase() === "true" || false,
     sessionSite: process.env.SESSION_SITE || 'https://session-toxxictech.zone.id',    
